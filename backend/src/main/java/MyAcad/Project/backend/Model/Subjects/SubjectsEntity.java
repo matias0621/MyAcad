3e71ebd46f5009fb594485feba6b4d75dfcf0292
matias0621 <mattiascorrea@outlook.com>
2025-10-17 16:44:42 -0300

    variables de entornos en el docker compose

--- backend/src/main/java/MyAcad/Project/backend/Model/Subjects/SubjectsEntity.java
+++ backend/src/main/java/MyAcad/Project/backend/Model/Subjects/SubjectsEntity.java
@@ -1,4 +1,27 @@
 package MyAcad.Project.backend.Model.Subjects;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SubjectsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subject_id")
+    private Long id;
+
+    private String name;
+    private String description;
+    private int semesters;
+
+    @OneToMany
+    @JoinColumn(name = "subject_id")
+    private List<SubjectsEntity> subjects_for_semester;
 }

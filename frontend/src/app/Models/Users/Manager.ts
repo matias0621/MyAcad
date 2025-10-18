0b011ac0ba54e1a3aacefc3098665b14a981580b
Tiago Jaen <tiagojaen123@gmail.com>
2025-10-18 18:08:43 -0300

    Header y Footer
    Prototipos de header y footer abiertos a cambios.

---
+++ frontend/src/app/Models/Users/Manager.ts
@@ -0,0 +1,8 @@
+export default interface Manager{
+    id: number;
+    name : string;
+    lastName : string;
+    email : string;
+    username : string;
+    password : string; 
+}
---
+++ frontend/src/app/Models/Users/Teachers.ts
@@ -0,0 +1,8 @@
+export default interface Teacher{
+    id: number;
+    name : string;
+    lastName : string;
+    email : string;
+    username : string;
+    password : string; 
+}

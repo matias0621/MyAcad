a0e7be6eac726a2e81f05d2c6ed7c5853d082339
Keila Ayelen Lima <keila_alima@hotmail.com>
2025-10-24 22:28:15 -0300

    creacion del modelo y servicio de commission, en proceso

---
+++ frontend/src/app/pages/commission/commission.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commission',
+  imports: [],
+  templateUrl: './commission.html',
+  styleUrl: './commission.css'
+})
+export class Commission {
+
+}

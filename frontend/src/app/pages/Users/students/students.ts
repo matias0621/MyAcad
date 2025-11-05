1f05606db173699c4195c589c67be99929bb7cc2
Tiago Jaen <tiagojaen123@gmail.com>
2025-11-04 22:01:32 -0300

    correcciones user form

--- frontend/src/app/pages/Users/students/students.ts
+++ frontend/src/app/pages/Users/students/students.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { UserList } from "../../../components/Users/user-list/user-list";
-import { UserEditForm } from "../../../components/Users/user-edit-form/user-edit-form";
-import { UserForm } from '../../../components/Users/user-form/user-form';
 
 @Component({
   selector: 'app-students',
-  imports: [UserForm, FormsModule, UserList, UserEditForm],
+  imports: [UserList],
   templateUrl: './students.html',
   styleUrl: './students.css'
 })

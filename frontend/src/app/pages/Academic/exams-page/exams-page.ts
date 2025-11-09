fabdf0c99c1488221feac1bf4297230b80f378d5
matias0621 <mattiascorrea@outlook.com>
2025-11-09 20:19:38 -0300

    Arreglando mis cagadas

--- frontend/src/app/pages/Academic/exams-page/exams-page.ts
+++ frontend/src/app/pages/Academic/exams-page/exams-page.ts
@@ -3,60 +3,113 @@ import { Component, OnInit } from '@angular/core';
 import { Exams, ExamsPost } from '../../../Models/Exam/Exam';
 import { ExamsService } from '../../../Services/Exams/exams-service';
 import { SubjectsService } from '../../../Services/Subjects/subjects-service';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ProgramService } from '../../../Services/program-service';
+import Program from '../../../Models/Program/Program';
+import Subjects from '../../../Models/Subjects/Subjects';
 
 @Component({
   selector: 'app-exams-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './exams-page.html',
   styleUrl: './exams-page.css',
 })
 export class ExamsPage implements OnInit {
   listExams: Exams[] = [];
+  listCareer: Program[] = [];
+  listSubjects: Subjects[] = [];
   form!: FormGroup;
   score!: FormControl;
   legajo!: FormControl;
   examType!: FormControl;
-  selectedExam ?: Exams;
+  careerName!: FormControl;
+  subjects!: FormControl;
+  selectedExam?: Exams;
 
-  idSubjects!: number 
-  examId!:number
+  idSubjects!: number;
+  examId!: number;
 
   constructor(
     private examsService: ExamsService,
     private subjectsService: SubjectsService,
     private notificationService: NotificationService,
-    private router:Router
-  ) {}
+    private programService: ProgramService,
+  ) {
+    this.legajo = new FormControl('', [Validators.required]);
+    this.examType = new FormControl('', [Validators.required]);
+    this.score = new FormControl('', [Validators.required]);
+    this.careerName = new FormControl('');
+    this.subjects = new FormControl('', [Validators.required]);
 
-  ngOnInit(): void {
-    this.getAllExam()
+    this.form = new FormGroup({
+      legajo: this.legajo,
+      score: this.score,
+      careerName: this.careerName,
+      subjects: this.subjects,
+      examType: this.examType,
+    });
   }
 
-  getAllExam(){
-    this.examsService.getAllExams().subscribe({
-      next: (res) => {
-        this.listExams = res;
-        console.log(res)
-        console.log(this.listExams)
-      },
-      error: (err) => {
-        console.log(err)
+  ngOnInit(): void {
+    this.getAllExam();
+    this.getAllCarrer()
+
+    // Cuando cambia la carrera seleccionada:
+    this.careerName.valueChanges.subscribe((careerName: string) => {
+      if (careerName) {
+        this.getSubjectsByNameProgram(careerName);
+      } else {
+        this.listSubjects = []; // limpiamos las materias si no hay carrera seleccionada
       }
     });
   }
 
-  addExam(){
-    this.router.navigate(['/create-exam'])
+  getAllCarrer() {
+    this.programService.getPrograms().subscribe({
+      next: (res) => {
+        this.listCareer = res;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
-  modifyExam(exam:Exams){
-    this.router.navigate(['/exams/', exam.id])
+  getAllExam() {
+    this.examsService.getAllExams().subscribe({
+      next: (res) => {
+        this.listExams = res;
+        console.log(res);
+        console.log(this.listExams);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
-  
-  deleteExam(examId:number){
+  getSubjectsByNameProgram(careerName: string) {
+    this.subjectsService.getByProgram(careerName).subscribe({
+      next: (res) => {
+        this.listSubjects = res;
+      },
+      error: (err) => {
+        console.error(err);
+        this.listSubjects = [];
+      },
+    });
+  }
+
+  modifyExam(exam: Exams) {
+    this.score.setValue(exam.score)
+    this.legajo.setValue(exam.student.legajo)
+    this.examType.setValue(exam.examType)
+    this.careerName.setValue(exam.subject.program)
+    this.subjects.setValue(exam.subject.id)
+  }
+
+  deleteExam(examId: number) {
     this.notificationService
       .confirm(
         '¿Estás seguro de que deseas eliminar esta comisión?',
@@ -89,5 +142,65 @@ export class ExamsPage implements OnInit {
 
   cleanForm() {
     this.form.reset();
+  }
+
+  
+
+  postExam() {
+
+    if (this.form.invalid) {
+      this.notificationService.warning(
+        'Formulario inválido. Por favor, complete todos los campos correctamente.'
+      );
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const examLoad: ExamsPost = {
+      score: this.score.value,
+      examType: this.examType.value,
+      legajoStudent: this.legajo.value,
+      subjectId: this.subjects.value,
+    };
+
+    this.examsService.postExam(examLoad).subscribe({
+      next: (data) => {
+        this.notificationService.success('Examen cdxagregado exitosamente');
+        this.getAllExam()
+        this.form.reset();
+      },
+      error: (error) => {
+        this.notificationService.error(error.error, true);
+        console.error(error);
+      },
+    });
+  }
+
+  putExam() {
+    if (this.form.invalid) {
+      this.notificationService.warning(
+        'Formulario inválido. Por favor, complete todos los campos correctamente.'
+      );
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const examLoad: ExamsPost = {
+      score: this.score.value,
+      examType: this.examType.value,
+      legajoStudent: this.legajo.value,
+      subjectId: this.subjects.value,
+    };
+
+    this.examsService.putExam(this.subjects.value, examLoad).subscribe({
+      next: (data) => {
+        this.notificationService.success('Examen actualizado exitosamente');
+        this.form.reset();
+      },
+      error: (error) => {
+        this.notificationService.error(error.error, true);
+        console.error(error);
+      },
+    });
   }
 }
